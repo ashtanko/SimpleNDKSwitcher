479ce9ccf32becbbc8575f2673ffd4cf42ba91a1
AdamLuisSean <adamluissean@gmail.com>
2017-07-16 15:35:04 +0300

    [WIP] Add fast switching function

--- switcher.sh
+++ switcher.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-DEFAULT_NDK_PATH=/Users/alexeyshtanko/Library/Android/sdk/ndk-bundle
-BUNDLES_NDK_PATH=/Users/alexeyshtanko/Library/Android/sdk/ndk-bundles
-
+DEFAULT_NDK_PATH=/Users/$USER/Library/Android/sdk/ndk-bundle
+BUNDLES_NDK_PATH=/Users/$USER/Library/Android/sdk/ndk-bundles
+NDK_BUILD_NUMBER=$1
 sh detector.sh $DEFAULT_NDK_PATH $BUNDLES_NDK_PATH
 
 bundles=()
@@ -26,10 +26,19 @@ echo "SELECT BUNDLE: $typeCounter $i"
 done
 
 function do_choose() {
+if [ ! "$NDK_BUILD_NUMBER" ]; then
     read -r event
+    NDK_BUILD_NUMBER=event
+    exit 1
+else
+    echo "$NDK_BUILD_NUMBER"
+fi
+
     BUNDLES_LENGHT=${#bundles[@]}
-    if [ "$event" -ge 1 -a "$event" -le $BUNDLES_LENGHT ]; then
-        echo "CHOOSED: ${bundles[$event-1]}"
+    if [ "$NDK_BUILD_NUMBER" -ge 1 -a "$NDK_BUILD_NUMBER" -le $BUNDLES_LENGHT ]; then
+        echo "CHOOSED: ${bundles[$NDK_BUILD_NUMBER-1]} ON PATH: $BUNDLES_NDK_PATH/${bundles[$NDK_BUILD_NUMBER-1]}"
+        export ANDROID_NDK="$BUNDLES_NDK_PATH/${bundles[$NDK_BUILD_NUMBER-1]}"
+        echo $ANDROID_NDK
     else
         echo "OUT OF RANGE, CHOOSE ANOTHER VALUE"
         do_choose
